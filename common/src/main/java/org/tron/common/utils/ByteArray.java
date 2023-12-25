2431873e43a676a439ea35931c98bf00fec2d2aa
sophiawang <1975054324@qq.com>
2023-12-25 23:29:14 +0800

    test all buildkite tasks

--- common/src/main/java/org/tron/common/utils/ByteArray.java
+++ common/src/main/java/org/tron/common/utils/ByteArray.java
@@ -103,7 +103,7 @@ public class ByteArray {
       objectOutputStream.flush();
       bytes = byteArrayOutputStream.toByteArray();
     } catch (IOException e) {
-      logger.error("objectToByteArray is considered as failed: " + e.getMessage(), e);
+      logger.error("objectToByteArray is consideredd as failed: " + e.getMessage(), e);
     }
     return bytes;
   }
